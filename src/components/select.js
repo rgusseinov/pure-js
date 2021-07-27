daae8c984d416335cd1568101382e258cfb7f9c4
Ruslan Gusseinov <r.gusseinov@mail.ru>
2021-07-27 23:59:47 +0600

    Working on plugin

--- src/components/select.js
+++ src/components/select.js
@@ -1,44 +1,62 @@
+const getSelectTemplate = (items, placeholder) => {
+  const text = (placeholder) ? placeholder : 'Please select item';
+  
+  const options = items.map(item => {
+    return `<div class="select__item" data-id="${item.id}"> ${item.name} </div>`;
+  });
+
+  return `<div class="select__header">
+            <span class="select__current"> ${placeholder} </span>
+            <div class="select__icon"> X </div>
+          </div>
+          <div class="select__body">${options.join('')}</div>`;
+}
+
 export class Select{
 
   constructor(element, options){
-    this.$el = element
+    this.$el = document.querySelector(element)
     this.options = options
     this.render();
-    this.init()
+    this.addEventListeners();
   }
 
   render(){
-    const items = this.options.map(item => {
-      return `<div class="select__item" id="${item.id}"> ${item.name} </div>`
-    }).join('')
-    document.querySelector('.select__body').insertAdjacentHTML('afterbegin', items);
+    const { data, placeholder } = this.options
+    this.$el.innerHTML = getSelectTemplate(data, placeholder);
+    this.$el.querySelector('.select__header').addEventListener('click', this.clickHandlerMain.bind(this));
   }
 
-  init(){
-    const activeItem = this.$el.querySelector('.select__current')
+  addEventListeners(){
     const items = this.$el.querySelectorAll('.select__item')
-
-    activeItem.addEventListener('click', (evt) => {
-      if (this.$el.classList.contains('is-active')){
-        this.$el.classList.remove('is-active');
-      } else {
-        this.$el.classList.add('is-active');
-      }
-    })
-
-    items.forEach(item => {
-      item.addEventListener('click', this.select.bind(this))
-    });
-
+    items.forEach(item => item.addEventListener('click', this.clickHandler.bind(this)));
   }
 
-  select(id){
-    const activeItem = this.$el.querySelector('.select__current')
-    activeItem.textContent = evt.target.textContent
+  clickHandlerMain(e){
+    if (this.$el.classList.contains('is-active')){
+      this.close()
+    } else {
+      this.open()
+    }
+  }
+
+  clickHandler(e){
+    const { id } = e.target.dataset
+    this.$el.querySelector('.select__current').textContent = e.target.textContent
+    this.close()
+  }
+
+  open(){
+    this.$el.classList.add('is-active');
   }
 
   close(){
+    this.$el.classList.remove('is-active');
+  }
 
+  select(id){
+    const findItem = this.$el.querySelector(`.select__item[data-id="${id}`)
+    this.$el.querySelector('.select__current').textContent = findItem.textContent.trim()
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import { Select } from "./components/select";
 
 
-const select = new Select(document.querySelector('.select'), [
-  {id: 1, name: 'React'},
-  {id: 2, name: 'Angular'},
-  {id: 3, name: 'Vue'},
-  {id: 4, name: 'Node JS'},
-  {id: 5, name: 'Redux'},
-]);
+const select = new Select('.select', {
+    placeholder: 'Need to select item',
+    selectedId: 3,
+    data: [
+      {id: 1, name: 'React'},
+      {id: 2, name: 'Angular'},
+      {id: 3, name: 'Vue'},
+      {id: 4, name: 'Node JS'},
+      {id: 5, name: 'Redux'},
+    ],
+});
+
+window.s = select
+
+/*
+
+open()
+close()
+select('2')
+clear()
+destroy()
+
+
+*/
