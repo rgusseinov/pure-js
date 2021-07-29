188312f3236f7a5f0c4fcba91aabfc0bd484150c
Ruslan Gusseinov <r.gusseinov@mail.ru>
2021-07-29 22:13:41 +0600

    Finished plugin

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const select = new Select('.select', {
     placeholder: 'Need to select item',
     selectedId: 3,
     data: [
-      {id: 1, name: 'Almaty'},
-      {id: 2, name: 'Astana'},
-      {id: 3, name: 'Shymkent'},
-      {id: 4, name: 'Atyrau'},
-      {id: 5, name: 'Aktau'},
+      {id: 1, name: 'Kazakhstan'},
+      {id: 2, name: 'Uzbekistan'},
+      {id: 3, name: 'Russia'},
+      {id: 4, name: 'Ukraine'},
+      {id: 5, name: 'Kyrgystan'},
+      {id: 6, name: 'Tadjikistan'},
+      {id: 7, name: 'Belarus'}
     ],
 });
