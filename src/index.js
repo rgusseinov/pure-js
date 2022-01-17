e3d2b7fe9b1777912c9199e360ff7959509a0146
= <r.@mail.ru>
2022-01-17 17:52:06 +0600

    feat: finish simple select and drag & drop files

--- src/index.js
+++ src/index.js
@@ -1,42 +1,6 @@
-// Algoritms 
+import upload from "./components/upload";
 
-// 1. Stack
-
-function Stack(){
-
-  this.count = 0;
-  this.storage = {};
-
-  this.push = function(value){
-    this.storage[this.count] = value
-    this.count ++;
-  }
-
-  this.pop = function(){
-    if (this.count === 0) return;
-    this.count --;
-    const result = this.storage[this.count]
-    delete this.storage[this.count]
-    return result
-  }
-
-  this.peek = function(){
-    return this.storage[this.count - 1]
-  }
-
-  this.size = function(){
-    return this.count
-  } 
-
-}
-
-const stack = new Stack();
-
-stack.push(5);
-stack.push(2);
-stack.push(10);
-console.log(stack)
-
-
-
-console.log(stack.peek())
+upload('#file', {
+  multi: true,
+  accept: [".jpg",".png", ".gif"]
+})
