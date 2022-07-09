659ffcc7d5c528f8143d76b55c4b224b23954eb8
Ruslan Gusseinov <r.gusseinov@mail.ru>
2022-07-09 12:21:21 +0600

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 export default class Validator {
   constructor(type) {
-    this.stringValidTypes = ["", null, undefined];
-    this.validTypes = { string: this.stringValidTypes };
-    this.activeTypes = this.validTypes[type];
-    this.options = {required: false, contains: null, minLength: null};
+    this.validTypes = type;
+    this.options = { required: false, contains: null, minLength: null };
+    this.rules = [];
   }
 
   string() {
@@ -14,43 +13,45 @@ export default class Validator {
     this.options.required = true;
   }
 
-  isValid(value = "") {
+  minLength(length) {
+    this.options.minLength = length;
+  }
+
+  isValid(value) {
     if (this.options.required) {
       if (!value) return false;
-      if (this.options.contains) {
-        return value.includes(this.options.contains);
-      }
-     
+
+      if (this.options.contains) return value.includes(this.options.contains);
+      if (this.options.minLength) return value.length >= this.options.minLength;
+
       return Boolean(value);
     } else {
-      if (this.options.contains){
+      if (this.options.contains) {
         if (!value) return false;
         return value.includes(this.options.contains);
       }
+      if (Number.isInteger(value)) return false;
+      if (this.options.minLength) return value.length >= this.options.minLength;
     }
-    return (this.activeTypes.includes(value) || !value != undefined);
+    return value != " ";
   }
   contains(value) {
     this.options.contains = value;
     return this;
   }
-  minLength(value){
-    if (!value) return false;
-    this.options.minLength = value.length;
-    return this;
-  }
 }
-
 const v = new Validator();
 const schema = v.string();
-console.log(schema.minLength(10)); // true
 
-
-/* console.log(schema.isValid("fghfgh")); // true
-console.log(schema.isValid()); // true
+console.log(schema.isValid(123));
+console.log(schema.isValid("Almaty")); // true
+console.log(schema.isValid("")); // true
 console.log(schema.isValid(null)); // true
 console.log(schema.isValid(undefined)); // true
 
+console.log("---Min length----");
+schema.minLength(5);
+console.log(schema.isValid("Ruslan")); // true
 
 schema.required();
 
@@ -60,10 +61,13 @@ console.log(schema.isValid("hexlet")); // true
 console.log(schema.isValid(null)); // false
 console.log(schema.isValid("")); // false
 
+console.log("---Min length----");
+schema.minLength(5);
+console.log(schema.isValid("what does the fox say")); // true
 
 console.log("---Contains----");
 console.log(schema.contains("what").isValid("what does the fox say")); // true;
-console.log(schema.contains('whatthe').isValid('what does the fox say')); // false;
-console.log(schema.isValid("what does the fox say")); // false */
- 
+console.log(schema.contains("whatthe").isValid("what does the fox say")); // false;
+console.log(schema.isValid("what does the fox say")); // false
 
+// console.log(Number.isInteger("122"));
